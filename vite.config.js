d951dccfebe7e9657f0ea361e18c7442ec4c84de
Mandar Kulkarni <kulkarnimandar702@gmail.com>
2026-04-09 23:40:46 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import process from "process";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ["store-feedback-platform.onrender.com"]
+  },
   server: {
-    host: true,             
+    host: true,
     port: Number(process.env.PORT) || 5173
   }
 });
